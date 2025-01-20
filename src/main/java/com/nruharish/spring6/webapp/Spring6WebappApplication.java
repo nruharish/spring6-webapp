b8dc91c55352cbbebd0dab695d84e31c7110d8e4
Harish BS <bs.harish@gmail.com>
2025-01-20 20:28:24 +0530

    New project

---
+++ src/main/java/com/nruharish/spring6/webapp/Spring6WebappApplication.java
@@ -0,0 +1,13 @@
+package com.nruharish.spring6.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6WebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6WebappApplication.class, args);
+	}
+
+}
